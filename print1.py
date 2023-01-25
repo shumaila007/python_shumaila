f5e32a3d5bbd3a17f5146898fbf6ac608309d2ba
shumaila007 <123533028+shumaila007@users.noreply.github.com>
2023-01-25 11:05:09 +0530

    Create print1.py

---
+++ print1.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints=np.array([1,2,6,8,99])
+ypoints=np.array([3,8,1,10,99])
+
+plt.plot(xpoints,ypoints)
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#xpoints=np.array([1,2,6,8,99])
+ypoints=np.array([3,8,1,10,5,7])
+
+plt.plot(ypoints)
+plt.show()
+
